a46f0b045072774d94736f106ec03746cf41d306
soohyun <vivian0304@naver.com>
2022-08-29 20:55:31 +0900

    feat:settings

---
+++ src/main/java/com/shanep/model/Board.java
@@ -0,0 +1,29 @@
+package com.shanep.model;
+
+import javax.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="board")
+@Getter
+@Setter
+@ToString
+public class Board {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer boardno;
+	
+	private String title;
+	private String content;
+	private String writer;
+
+//	@OneToMany(mappedBy = "board")
+//	List<Comment> comment = new ArrayList<>();
+}
---
+++ src/main/java/com/shanep/model/User.java
@@ -0,0 +1,37 @@
+package com.shanep.model;
+
+import javax.persistence.*;
+
+import com.shanep.enumpkg.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Entity
+@Table(name="users")
+public class User extends TimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 30, unique = true)
+    private String username; // 아이디
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
