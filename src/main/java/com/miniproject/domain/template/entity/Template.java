1af691f05e8f59a3d287496cf7668bd7a1a33b7b
hyeonjin <hyeonjin9819@gmail.com>
2022-09-20 07:37:40 +0900

    fix/template2
    탬플릿 수정

--- src/main/java/com/miniproject/domain/template/entity/Template.java
+++ src/main/java/com/miniproject/domain/template/entity/Template.java
@@ -1,12 +1,7 @@
 package com.miniproject.domain.template.entity;
 
 import com.miniproject.global.entity.TimeEntity;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
+import lombok.*;
 
 import javax.persistence.*;
 
@@ -22,29 +17,27 @@ import javax.persistence.*;
  * @update-description :
  */
 @Entity
-@Getter
-@SQLDelete(sql = "UPDATE template SET is_deleted = true WHERE id = ?")
-@Where(clause = "is_deleted = false") // 검색에 대한 filter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
 @Setter
+@Table(name = "template")
 @ToString
-public class Template extends TimeEntity{
-
+public class Template extends TimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column
-    private String fileName;
-
-    @Column
+    @Column(nullable = false)
     private String fileUrl;
 
-    @Column
+    @Column(nullable = false)
+    @Builder.Default
     private boolean isDeleted = Boolean.FALSE;
 
-    public Template(String fileName, String fileUrl) {
-        this.fileName = fileName;
-        this.fileUrl = fileUrl;
-    }
+//    public Template(String fileName, String fileUrl) {
+//        this.fileName = fileName;
+//        this.fileUrl = fileUrl;
+//    }
 }
--- src/main/java/com/miniproject/domain/template/repository/TemplateRepository.java
+++ src/main/java/com/miniproject/domain/template/repository/TemplateRepository.java
@@ -2,7 +2,11 @@ package com.miniproject.domain.template.repository;
 
 import com.miniproject.domain.template.entity.Template;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,7 +21,13 @@ import java.util.List;
  * @update-author : 000
  * @update-description :
  */
+
 @Repository
 public interface TemplateRepository extends JpaRepository<Template, Integer> {
     public List<Template> findAllByOrderByIdDesc();
+
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query(value = "UPDATE template SET is_deleted = true WHERE id = CAST(:id as INTEGER)", nativeQuery = true)
+    void deleteById(@Param("id") int id);
 }
